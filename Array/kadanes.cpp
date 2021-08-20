2def41fec00806f46b35a795d983a70a49dda7d6
Kapilshanbhag09 <shanbhag.kapil09@gmail.com>
2021-08-20 12:22:50 +0530

    Added Kadanes Algo

---
+++ Array/kadanes.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int sum=0;
+    int max=INT_MIN;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        if(sum>max){
+            max=sum;
+        }
+        if(sum<0){
+            sum=0;
+        }
+
+    }
+    cout<<max<<endl;
+}
